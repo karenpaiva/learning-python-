73f8fd40a686f44982432e0211f2be4137b52667
karenpaiva <57016155+karenpaiva@users.noreply.github.com>
2019-11-01 15:09:36 -0300

    Add files via upload

---
+++ Introdução a programanção com python.py
@@ -0,0 +1,53 @@
+#####Capítulo 1#####
+#No IDLE o que for escrito deve ser salvo antes de realizar a ação
+#Atalho: control+S seguido de F5
+#O F5 executa
+# A primeira função mais utilizada é a função print
+# Com ele iremos exibir algo na tela 
+# O que for escrito deve estar entre parenteses
+print(5)
+print('Oi!')
+#Variáveis receber atribuições
+#Há 4 tipos de variáveis. As inteiras, as flot,as string e as lógicas 
+#Cria variável do tipo inteiro
+x = 1 # x recebe o 1
+#Cria variável do tipo float
+y = 3.13
+#Cria variável do tipo string
+#As strigs  devem estar entre aspas simples ou duplas
+m = "Python"
+m = 'Python'
+#Cria variável do tipo lógica
+w = True  
+y = False
+print(y)
+# As variáveis podem receber novos valores
+x=1
+print(x)
+x=4
+print(x)
+#Elas são usadas para facilitar a atribuição de um valor
+salário=1500
+aumento=5
+print(salário+(salário*aumento/100))
+# Se o salário for alterado será obtido um novo resultado
+#Exercícios do capítulo 1
+#Um programa para exibir o nome na tela
+print(input('Qual seu nome:'))#ou
+print('Karen')
+#Um programa que exiba o resultado de 2a*3b onde a=3 e b=5
+a=3
+b=5
+print((2*a)*(3*b))
+#Soma de 3 variáveis
+print(a+b+x)
+#Calcular o aumento de 15% no salário de 750
+salário=750
+aumento=15
+print(salário+(salário*aumento/100))
+#float é igual a inteiro
+x=5.00
+b=5
+print(x==b)
+
+
